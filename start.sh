ef3057778dce2b9087559286c81a5fd0dbb0e51a
innermost47 <anthony.charretier@gmail.com>
2024-10-09 18:51:50 +0200

    expand timeout

--- start.sh
+++ start.sh
@@ -15,6 +15,6 @@ if [ "$FLASK_ENV" == "development" ]; then
     flask run --host=127.0.0.1 --port=5000
 else
     echo "Starting the application in production mode..."
-    waitress-serve --listen=127.0.0.1:5000 app:app
+    waitress-serve --listen=127.0.0.1:5000 --channel-timeout=3600 app:app
 fi
 
